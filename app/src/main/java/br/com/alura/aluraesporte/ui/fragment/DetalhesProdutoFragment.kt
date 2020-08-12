9565e11990ae449efc9d385d38f1f573b2e5969a
Robson Lima <rsl.robson@gmail.com>
2020-08-12 15:52:19 -0300

    Navegando entre destinos e enviando dados entre destinos/fragments

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
 import br.com.alura.aluraesporte.model.Produto
 import br.com.alura.aluraesporte.ui.activity.CHAVE_PRODUTO_ID
 import br.com.alura.aluraesporte.ui.viewmodel.DetalhesProdutoViewModel
 import kotlinx.android.synthetic.main.detalhes_produto.*
-import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -45,7 +45,12 @@ class DetalhesProdutoFragment : Fragment() {
 
     private fun configuraBotaoComprar() {
         detalhes_produto_botao_comprar.setOnClickListener {
-            viewModel.produtoEncontrado.value?.let(quandoProdutoComprado)
+            viewModel.produtoEncontrado.value?.let {
+                val controlador = findNavController()
+                val dados = Bundle()
+                dados.putLong(CHAVE_PRODUTO_ID, produtoId)
+                controlador.navigate(R.id.pagamento, dados)
+            }
         }
     }
 
