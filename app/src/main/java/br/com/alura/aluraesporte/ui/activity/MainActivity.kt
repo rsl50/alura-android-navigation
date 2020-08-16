4a6f56c484d363d0b530b1eac4fa19d5be4865ba
Robson Lima <rsl.robson@gmail.com>
2020-08-15 22:35:19 -0300

    Adicionando listener na navegação, alterando título da tela conforme fragment e controlando a exibição da supportActionBar

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -1,14 +1,34 @@
 package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val controlador by lazy {
+        findNavController(R.id.main_activity_nav_host)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        controlador.addOnDestinationChangedListener { controller,
+                                                      destination,
+                                                      arguments ->
+
+            // Coloca o label do Fragment como título do Fragment na tela
+            title = destination.label
+
+            // Controla exibição da ActionBar conforme a tela
+            when(destination.id) {
+                R.id.listaProdutos -> supportActionBar?.show()
+                R.id.login -> supportActionBar?.hide()
+            }
+        }
     }
 
 }
