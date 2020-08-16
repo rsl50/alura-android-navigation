adaa8a0d3ed8e3660ff6baa9819c989aa0b89705
Robson Lima <rsl.robson@gmail.com>
2020-08-16 15:42:29 -0300

    Implementando o ViewModel para o estado do App

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -1,16 +1,19 @@
 package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val controlador by lazy {
         findNavController(R.id.main_activity_nav_host)
     }
+
+    private val viewModel: EstadoAppViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
